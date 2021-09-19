f0abb5c7def9f05aa164096255e5109141d22167
VanillaBrooks <brooks@karlik.org>
2021-09-19 14:54:07 -0700

    update to latest ruma version

---
+++ src/lib.rs
@@ -0,0 +1,200 @@
+use ruma::api::client::r0::media::create_content;
+use ruma::api::client::r0::membership::{get_member_events, joined_rooms};
+use ruma::api::client::r0::message::send_message_event;
+use ruma::api::client::r0::room::create_room;
+use ruma::events::room::member::MembershipState;
+use ruma::identifiers::{RoomId};
+use std::convert::TryFrom;
+use std::io::Read;
+
+pub use ruma::identifiers::UserId;
+
+use ruma::events as ruma_events;
+
+pub type HyperClient = ruma::client::http_client::HyperNativeTls;
+pub type Client = ruma::client::Client<HyperClient>;
+type RumaClientError = ruma::client::Error<hyper::Error, ruma::api::client::Error>;
+
+pub async fn send_text_message(client: &Client, text: String, target_user: UserId, self_id: UserId) -> Result<(), Error> {
+    let room_id = get_room_id(client, target_user, self_id).await?;
+
+    let txn_id = String::new();
+
+    let data = text_event(text);
+
+    let text_request = send_message_event::Request::new( &room_id, &txn_id, &data);
+
+    client.send_request(text_request).await?;
+
+    Ok(())
+}
+
+pub fn client(homeserver_url: String) -> Client {
+    let https = hyper_tls::HttpsConnector::new();
+    let client = hyper::Client::builder().build::<_, hyper::Body>(https);
+
+    Client::with_http_client(client, homeserver_url, None)
+}
+
+fn text_event(text: String) -> ruma_events::AnyMessageEventContent {
+    let text = ruma_events::room::message::TextMessageEventContent::plain(text);
+    let msg_type = ruma_events::room::message::MessageType::Text(text);
+    let message_event = ruma_events::room::message::MessageEventContent::new(msg_type);
+    ruma_events::AnyMessageEventContent::RoomMessage(message_event)
+}
+
+
+pub async fn send_attachment(client: &Client, attachment_path: &str, description: Option<String>, target_user: UserId, self_id: UserId) -> Result<(), Error> {
+    let room_id = get_room_id(client, target_user, self_id).await?;
+
+    let mime = mime_guess::from_path(attachment_path)
+        .first_or_octet_stream()
+        .essence_str()
+        .to_string();
+
+    let pathbuf = std::path::PathBuf::from(attachment_path);
+
+    let filename = pathbuf
+        .file_name()
+        .ok_or(Error::MissingFilename)?
+        .to_str()
+        .unwrap_or("file")
+        .to_string();
+
+    let mut reader = std::fs::File::open(&pathbuf)?;
+    let mut bytes = Vec::new();
+    reader.read_to_end(&mut bytes)?;
+
+    let size = bytes.len();
+
+    let upload_request = create_content::Request::new(&bytes);
+
+    let uri = client.send_request(upload_request).await?.content_uri;
+
+    let description = description.unwrap_or("".to_string());
+
+    // TODO: load info into the struct here using the above data
+    let info = None;
+
+    let file = ruma_events::room::message::FileMessageEventContent::plain(description, uri, info);
+    let msg_type = ruma_events::room::message::MessageType::File(file);
+    let message_event = ruma_events::room::message::MessageEventContent::new(msg_type);
+    let any_file_event = ruma_events::AnyMessageEventContent::RoomMessage(message_event);
+
+    let txn_id = String::new();
+
+    let file_request = send_message_event::Request::new(&room_id, &txn_id, &any_file_event);
+
+    client.send_request(file_request).await?;
+
+    Ok(())
+}
+
+async fn get_room_id(client: &Client, target_user: UserId, self_id: UserId) -> Result<RoomId, Error> {
+    let room_id = if let Some(id) = find_room(client, target_user.clone(), self_id).await? {
+        id
+    } else {
+        create_room(client, target_user).await?
+    };
+
+    Ok(room_id)
+}
+
+async fn find_room(
+    client: &Client,
+    target_user: UserId,
+    self_user_id: UserId,
+) -> Result<Option<RoomId>, Error> {
+    let mut user_room = None;
+
+    let rooms = joined_rooms::Request::new();
+    let rooms_response : joined_rooms::Response = client.send_request(rooms).await?;
+
+    for joined_room in rooms_response.joined_rooms.into_iter() {
+        let membership_request = get_member_events::Request::new(&joined_room);
+
+        let membership_response = client.send_request(membership_request).await?;
+
+        let mut target_not_leave = true;
+
+        for chunk in membership_response.chunk {
+            let chunk = chunk.deserialize()?;
+            if chunk.sender == self_user_id {
+                continue;
+            } else if chunk.sender == target_user {
+                match chunk.content.membership {
+                    MembershipState::Ban => {
+                        target_not_leave = false;
+                        break;
+                    }
+                    MembershipState::Leave => {
+                        target_not_leave = false;
+                        break;
+                    }
+                    _ => (),
+                }
+            } else {
+                break;
+            }
+        }
+
+        if target_not_leave {
+            user_room = Some(joined_room);
+            break;
+        }
+    }
+
+    Ok(user_room)
+}
+
+fn make_name<'a>(x: &'a str) ->  Result<&'a ruma::identifiers::RoomName, ruma::identifiers::Error>{
+    TryFrom::try_from(x)
+}
+
+async fn create_room(client: &Client, target_user: UserId) -> Result<RoomId, Error> {
+    let name  = "compute-notify";
+    let room_name = make_name(name).unwrap();
+
+    let creation_content = ruma::api::client::r0::room::create_room::CreationContent::new();
+
+    //we must now create a room and send messages to it
+    let mut create_room_request = create_room::Request::new();
+    let target=  &[target_user];
+
+    create_room_request.creation_content = creation_content;
+    create_room_request.initial_state= &[];
+    create_room_request.invite= target;
+    create_room_request.invite_3pid= &[];
+    create_room_request.is_direct= true;
+    create_room_request.name= Some(&room_name);
+    create_room_request.power_level_content_override= None;
+    create_room_request.preset= Some(create_room::RoomPreset::PrivateChat);
+    create_room_request.room_alias_name= None;
+    create_room_request.room_version= None;
+    create_room_request.topic= None;
+    create_room_request.visibility= ruma::api::client::r0::room::Visibility::Private;
+
+    let response = client.send_request(create_room_request).await?;
+
+    Ok(response.room_id)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Failed to parse a url from the provided homeserver url in the .config.json")]
+    UrlError(#[from] url::ParseError), //
+    #[error("Failed to parse a matrix user id from `{0}`. Example user id: @username:matrix-org")]
+    UsernameErr(String),
+    #[error("The provided attachment path was a directory, not a file")]
+    MissingFilename,
+    #[error("Error opening the provided attachment path: `{0}`")]
+    IoError(#[from] std::io::Error),
+    #[error("Could not deserialize config file: `{0}`")]
+    SerdeError(#[from] serde_json::error::Error),
+    #[error("Error with the client: `{0}`")]
+    RumaClient(#[from] RumaClientError),
+    #[error("Could not resolve identifier: `{0}`")]
+    RumaIdentifier(#[from] ruma::identifiers::Error),
+    #[error("There was a hyper error: `{0}`")]
+    HyperError(#[from] ruma::client::Error<hyper::Error, ruma::api::client::r0::uiaa::UiaaResponse>)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use ruma_client::Client;
-use ruma_client_api::r0::media::create_content;
-use ruma_client_api::r0::membership::{get_member_events, joined_rooms};
-use ruma_client_api::r0::message::create_message_event;
-use ruma_client_api::r0::room::create_room;
-use ruma_events::room::member::MembershipState;
-use ruma_identifiers::UserId;
+use matrix_notify::Error;
+
+use ruma::identifiers::UserId;
 use std::convert::TryFrom;
-use std::io::Read;
 
 #[derive(argh::FromArgs)]
 /// Send matrix messages and attachments to specified users
@@ -45,6 +40,7 @@ struct Attachment {
     #[argh(positional)]
     /// content of the message to send
     path: String,
+
 }
 
 #[tokio::main]
@@ -63,235 +59,24 @@ async fn inner_main() -> Result<(), Error> {
 
     let config = ConfigInfo::new()?;
 
-    let client = Client::https(url::Url::parse(&config.homeserver_url)?, None);
+    let client = matrix_notify::client(config.homeserver_url);
 
     client
-        .log_in(config.matrix_username, config.matrix_password, None, None)
+        .register_user(Some(&config.matrix_username), &config.matrix_password)
         .await?;
 
     //leave_all_rooms(&client).await.unwrap();
 
-    let mut user_room = None;
-
-    let rooms = joined_rooms::Request {};
-    let rooms_response = client.request(rooms).await?;
-
-    for joined_room in rooms_response.joined_rooms.into_iter() {
-        let membership_request = get_member_events::Request {
-            room_id: joined_room.clone(),
-            at: None,
-            membership: None,
-            not_membership: None,
-        };
-        let membership_response = client.request(membership_request).await?;
-
-        let mut target_not_leave = true;
-
-        for chunk in membership_response.chunk {
-            let chunk = chunk.deserialize()?;
-            if chunk.sender == self_user_id {
-                continue;
-            } else if chunk.sender == target_user {
-                match chunk.content.membership {
-                    MembershipState::Ban => {
-                        target_not_leave = false;
-                        break;
-                    }
-                    MembershipState::Leave => {
-                        target_not_leave = false;
-                        break;
-                    }
-                    _ => (),
-                }
-            } else {
-                break;
-            }
-        }
-
-        if target_not_leave {
-            user_room = Some(joined_room);
-            break;
-        }
-    }
-
-    // fetch the room ID that the user is currently in
-    let room_id = if let Some(room_id) = user_room {
-        // send message to this room
-        room_id
-    } else {
-        //we must now create a room and send messages to it
-        let create_room_request = create_room::Request {
-            creation_content: None,
-            initial_state: vec![],
-            invite: vec![target_user],
-            invite_3pid: vec![],
-            is_direct: Some(true),
-            name: Some("compute-notify".to_string()),
-            power_level_content_override: None,
-            preset: Some(create_room::RoomPreset::PrivateChat),
-            //room_alias_name: Some("compute-notify".to_string()),
-            room_alias_name: None,
-            room_version: None,
-            topic: None,
-            visibility: None,
-        };
-
-        let response = client.request(create_room_request).await?;
-
-        response.room_id
-    };
-
-    let txn_id = String::new();
-
     match args.subcommands {
         Subcommands::Text(text) => {
-            let data = TextMessage::new(text.text);
-            let text_request = create_message_event::Request {
-                room_id,
-                event_type: ruma_events::EventType::RoomMessage,
-                txn_id,
-                data: data.to_raw(),
-            };
-
-            client.request(text_request).await?;
+            matrix_notify::send_text_message(&client, text.text, target_user, self_user_id).await?;
         }
         Subcommands::Attachment(attachment) => {
-            let mime = mime_guess::from_path(&attachment.path)
-                .first_or_octet_stream()
-                .essence_str()
-                .to_string();
-            let pathbuf = std::path::PathBuf::from(attachment.path);
-            let filename = pathbuf
-                .file_name()
-                .ok_or(Error::MissingFilename)?
-                .to_str()
-                .unwrap_or("file")
-                .to_string();
-
-            let mut reader = std::fs::File::open(&pathbuf)?;
-            let mut bytes = Vec::new();
-            reader.read_to_end(&mut bytes)?;
-            let size = bytes.len();
-
-            let upload_request = create_content::Request {
-                filename: Some(filename.clone()),
-                content_type: mime.clone(),
-                file: bytes,
-            };
-
-            let uri = client.request(upload_request).await?.content_uri;
-
-            let attachment = AttachmentMessage::new(uri, size, mime, filename)?;
-
-            let file_request = create_message_event::Request {
-                room_id,
-                event_type: ruma_events::EventType::RoomMessage,
-                txn_id,
-                data: attachment.to_raw(),
-            };
-
-            client.request(file_request).await?;
+            matrix_notify::send_attachment(&client, &attachment.path, None, target_user, self_user_id).await?;
         }
     }
 
     Ok(())
-}
-
-#[derive(serde::Serialize)]
-struct TextMessage {
-    body: String,
-    format: &'static str,
-    formatted_body: String,
-    msgtype: &'static str,
-}
-impl TextMessage {
-    fn new(body: String) -> Self {
-        Self {
-            body: body.clone(),
-            format: "org.matrix.custom.html",
-            formatted_body: body,
-            msgtype: "m.text",
-        }
-    }
-
-    fn to_raw(self) -> Box<serde_json::value::RawValue> {
-        let string = serde_json::to_string(&self).unwrap();
-        serde_json::value::RawValue::from_string(string).unwrap()
-    }
-}
-
-#[derive(serde::Serialize)]
-struct AttachmentMessage {
-    body: String,
-    ///The original filename of the uploaded file.
-    filename: String,
-    ///Information about the file referred to in url.
-    info: FileInfo,
-    msgtype: &'static str,
-    url: String,
-}
-impl AttachmentMessage {
-    fn new(file_url: String, size: usize, mime: String, filename: String) -> Result<Self, Error> {
-        let info = FileInfo {
-            mimetype: mime,
-            size: size,
-            thumbnail_url: None,
-            thumbnail_info: None,
-        };
-
-        Ok(Self {
-            body: filename.clone(),
-            ///The original filename of the uploaded file.
-            filename: filename,
-            ///Information about the file referred to in url.
-            info,
-            msgtype: "",
-            url: file_url,
-        })
-    }
-
-    fn to_raw(self) -> Box<serde_json::value::RawValue> {
-        let string = serde_json::to_string(&self).unwrap();
-        serde_json::value::RawValue::from_string(string).unwrap()
-    }
-}
-
-#[derive(serde::Serialize)]
-struct FileInfo {
-    mimetype: String,
-    size: usize,
-    thumbnail_url: Option<String>,
-    thumbnail_info: Option<String>,
-}
-
-//#[allow(dead_code)]
-//async fn leave_all_rooms(TODO) -> Result<(), Error> {
-// let req = ruma_client_api::r0::membership::leave_room::Request {
-//     room_id: joined_room,
-// };
-
-// client.request(req).await?;
-//    Ok(())
-//}
-
-#[derive(Debug, thiserror::Error)]
-enum Error {
-    #[error("A generic matrix api error occured. Full error: `{0}`")]
-    MatrixError(#[from] ruma_client::Error<ruma_client_api::Error>),
-    #[error("Failed to parse a url from the provided homeserver url in the .config.json")]
-    UrlError(#[from] url::ParseError), //
-    #[error("Failed to parse a matrix user id from `{0}`. Example user id: @username:matrix-org")]
-    UsernameErr(String),
-    #[error("The provided attachment path was a directory, not a file")]
-    MissingFilename,
-    #[error("Error opening the provided attachment path: `{0}`")]
-    IoError(#[from] std::io::Error),
-    #[error("Could not deserialize config file: `{0}`")]
-    SerdeError(#[from] serde_json::error::Error),
-    #[error("adf")]
-    DeserializeError(#[from] ruma_events::InvalidEvent),
-    #[error("adf")]
-    IdentifiersError(#[from] ruma_identifiers::Error),
 }
 
 #[derive(serde::Deserialize)]
@@ -300,6 +85,7 @@ struct ConfigInfo {
     matrix_password: String,
     homeserver_url: String,
 }
+
 impl ConfigInfo {
     fn new() -> Result<Self, Error> {
         let bytes = include_bytes!("../.config.json");
